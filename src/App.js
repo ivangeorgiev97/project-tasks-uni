251e03f985002a4129ede61fb54ae7d9b33f68de
ivangeorgiev97 <georgiev1313@abv.bg>
2020-04-10 15:52:56 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+
+function App() {
+  return <div className="App">TODO- Add layouts- main, header and footer</div>;
+}
+
+export default App;
